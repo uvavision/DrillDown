b22b1a6686447a88b3be05dceff845476fd4ad70
FUWEN TAN <fuwen.tan@gmail.com>
2019-10-17 22:43:35 -0400

    evaluation of the paragraph model

--- lib/modules/paragraph_model.py
+++ lib/modules/paragraph_model.py
@@ -14,12 +14,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+from modules.grounding_loss import GroundingLoss
+from modules.ranker import Ranker
+
+
 class ParagraphModel(nn.Module):
     def __init__(self, config):
         super(ParagraphModel, self).__init__()
         self.cfg = config
         self.txt_enc = TextEncoder(self.cfg)
         self.img_enc = RegionEncoder(self.cfg)
+        self.criterion = GroundingLoss(self.cfg)
+        self.ranker = Ranker(self.cfg)
         self.init_weights()
 
     def init_weights(self):
@@ -30,6 +36,33 @@ class ParagraphModel(nn.Module):
                 nn.init.uniform_(param)
             elif 'weight' in name:
                 nn.init.xavier_uniform_(param)
+        
+    def loss(self, img_feats, img_masks, txt_feats):
+        # img_feats: (bsize, nregions, fsize)
+        # img_masks: (bsize, nregions)
+        # txt_feats: (bsize, 1, fsize)
+        similarities = self.criterion.compute_batch_mutual_similarity(img_feats, img_masks, txt_feats)
+        losses = self.criterion.contrastive_loss(similarities[:,:,0])
+        return losses
+
+    def evaluate(self, img_feats, img_masks, txt_feats):
+        bsize = txt_feats.size(0)
+        gt_inds = torch.from_numpy(np.array(list(range(bsize)))).long()
+        if self.cfg.cuda:
+            gt_inds = gt_inds.cuda()
+        ranks, top5_inds = self.ranker.compute_rank(txt_feats, img_feats, img_masks, gt_inds)
+        ssize, nturns = ranks.size()
+        metrics = {}
+        for turn in range(nturns):
+            ranks_np = ranks[:, turn].cpu().data.numpy()
+            r1 = 100.0 * len(np.where(ranks_np < 1)[0])/ssize
+            r5 = 100.0 * len(np.where(ranks_np < 5)[0])/ssize
+            r10 = 100.0 * len(np.where(ranks_np < 10)[0])/ssize
+            medr = np.floor(np.median(ranks_np)) + 1
+            meanr = ranks_np.mean() + 1
+            metrics[turn] = (np.array([r1, r5, r10, medr, meanr]).astype(np.float64)).tolist()
+        top5_inds = top5_inds.cpu().data.numpy()
+        return metrics, top5_inds
 
     def forward(self, sent_inds, sent_msks, region_feats, region_clses, region_masks):
         ###########################################################################################
