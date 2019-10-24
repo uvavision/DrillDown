be17f76c0c3c45876502a650fc001376dbde8e30
FUWEN TAN <fuwen.tan@gmail.com>
2019-10-23 20:58:39 -0400

    image, rankfusion

--- lib/modules/image_hred_model.py
+++ lib/modules/image_hred_model.py
@@ -31,6 +31,9 @@ class ImageHREDModel(nn.Module):
         return loss
 
     def evaluate(self, img_feats, txt_feats):
+        if self.cfg.rank_fusion:
+            return self.evaluate_rankfusion(img_feats, txt_feats)
+
         ssize, nturns, fsize = txt_feats.size()
         ssize, fsize = img_feats.size()
         gt_inds = torch.arange(0, ssize).long().view(-1, 1)
@@ -51,6 +54,40 @@ class ImageHREDModel(nn.Module):
             medr = np.floor(np.median(ranks)) + 1
             meanr = ranks.mean() + 1
             metrics[turn] = (np.array([r1, r5, r10, medr, meanr]).astype(np.float64)).tolist()
+        return metrics, caches_results
+
+    def evaluate_rankfusion(self, img_feats, txt_feats):
+        ssize, nturns, fsize = txt_feats.size()
+        ssize, fsize = img_feats.size()
+        gt_inds = torch.arange(0, ssize).long().view(-1, 1)
+        if self.cfg.cuda:
+            gt_inds = gt_inds.cuda()
+
+        metrics = {}
+        caches_results = {}
+        ranks_per_turns = []
+
+        for turn in range(nturns):
+            curr_txt_feats = txt_feats[:, turn]
+            sim = curr_txt_feats.mm(img_feats.t())
+            sim = sim.cpu().data.numpy()
+            ranks = [np.array([stats.percentileofscore(sim[i], j) for j in sim[i]]) for i in range(len(sim))]
+            ranks_per_turns.append(np.stack(ranks, 0))
+        ranks_per_turns = np.stack(ranks_per_turns, -1)
+        accu_ranks = ranks_per_turns.copy()
+        for turn in range(1, nturns):
+            accu_ranks[:,:,turn] = np.mean(ranks_per_turns[:,:,:(turn+1)], -1)
+        for turn in range(nturns):
+            sorted_inds = torch.argsort(accu_ranks, dim=-1, descending=True) 
+            caches_results[turn] = sorted_inds[:,:5].cpu().data.numpy()
+            ranks = torch.argmax(torch.eq(sorted_inds, gt_inds).float(), dim=-1)
+            ranks = ranks.cpu().data.numpy()
+            r1 = 100.0 * len(np.where(ranks < 1)[0])/ssize
+            r5 = 100.0 * len(np.where(ranks < 5)[0])/ssize
+            r10 = 100.0 * len(np.where(ranks < 10)[0])/ssize
+            medr = np.floor(np.median(ranks)) + 1
+            meanr = ranks.mean() + 1
+            metrics[turn] = (np.array([r1, r5, r10, medr, meanr]).astype(np.float64)).tolist()       
         return metrics, caches_results
 
     def demo_step(self, sentence, all_captions, img_feats, db, gt_ind=0):
